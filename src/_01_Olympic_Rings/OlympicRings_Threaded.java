05478991be18ea81cf00d7e44ec0f24dd8bd3518
league <league@imac-22.attlocal.net>
2023-07-10 16:55:54 -0700

    Olympic rings and Advanced Robot race:IP:

--- src/_01_Olympic_Rings/OlympicRings_Threaded.java
+++ src/_01_Olympic_Rings/OlympicRings_Threaded.java
@@ -1,8 +1,58 @@
 package _01_Olympic_Rings;
 
+import java.util.Iterator;
 
-public class OlympicRings_Threaded {
+import org.jointheleague.graphical.robot.Robot;
+
+public class OlympicRings_Threaded implements Runnable{
+	static boolean bol1 = false;
+	static boolean bol2 = false;
+	static boolean bol3 = false;
 	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+	public static void main(String[] args) {
+	
+		
+		
+	   
+	    	Thread r1 = new Thread(new OlympicRings_Threaded());
+	    	r1.start();
+	    	bol1 = true;
+	    	Thread r2 = new Thread(new OlympicRings_Threaded());
+	    	r2.start();
+	    	bol2 = true;
+	    	Thread r3 = new Thread(new OlympicRings_Threaded());
+	    	r3.start();
+	    	bol3 = true;
+	    	
+		}
+		
+		
+		
+		
+		
 
+
+	@Override
+	public void run() {
+		Robot timmy = new Robot();
+	if(bol1) {
+		 timmy.moveTo(400, 700);
+		 bol1 = false;
+	}else if(bol2) {
+		timmy.moveTo(450, 700);
+		 bol2 = false;
+	}else if(bol3){
+		timmy.moveTo(425, 650);
+		bol3 = false;
+	}
+		
+		timmy.setSpeed(10);
+		for (int i = 0; i < 25; i++) {
+			timmy.penDown();
+			timmy.turn(i);
+			timmy.move(i);
+		}
+		
+	}
 }
 
--- src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
+++ src/_02_Advanced_Robot_Race/AdvancedRobotRace.java
@@ -1,6 +1,73 @@
 package _02_Advanced_Robot_Race;
 
-public class AdvancedRobotRace {
-	// Re-do the robot race recipe from level 3 module 0. 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+import _01_Olympic_Rings.OlympicRings_Threaded;
+
+public class AdvancedRobotRace implements Runnable {
+	// Re-do the robot race recipe from level 3 module 0.
 	// This time, use threads to make all of the robots go at the same time.
+	public static void main(String[] args) {
+		Thread r1 = new Thread(new AdvancedRobotRace());
+    	r1.start();
+ //   	bol1 = true;
+    	Thread r2 = new Thread(new AdvancedRobotRace());
+    	r2.start();
+ //    	bol2 = true;
+    	Thread r3 = new Thread(new AdvancedRobotRace());
+    	r3.start();
+ // 	bol3 = true;
+    	Thread r4 = new Thread(new AdvancedRobotRace());
+    	r4.start();
+ // 	bol4 = true;
+    	Thread r5 = new Thread(new AdvancedRobotRace());
+    	r5.start();
+ // 	bol5 = true;
+		Robot[] robots = new Robot[5];
+
+		for (int i = 0; i < robots.length; i++) {
+
+			robots[i] = new Robot();
+			robots[i].setY(540);
+			robots[i].setX(100 + (i * 100));
+			robots[i].setSpeed(150);
+
+		}
+
+		boolean raceInProgress = true;
+		boolean firstRobot = false;
+		int WinningRobot = -1;
+		while (raceInProgress) {
+			for (int i = 0; i < robots.length; i++) {
+				if (robots[i].getY() < 0) {
+					raceInProgress = false;
+					WinningRobot = i;
+					JOptionPane.showMessageDialog(null, "The winning robot is robot number " + WinningRobot + "!");
+					robots[WinningRobot].setSpeed(100);
+					robots[WinningRobot].turn(180);
+					robots[WinningRobot].move(25);
+					robots[WinningRobot].turn(180);
+					break;
+
+				}
+
+				Random rand = new Random();
+				int p = rand.nextInt(50);
+				robots[i].move(p);
+				robots[i].setSpeed(150);
+
+			}
+		}
+
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
